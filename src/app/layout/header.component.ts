b8e1b41320ce1f63e7df211907fd521244450a7f
eduard0708 <eduard.uytoco@gmail.com>
2021-09-22 02:05:06 +0300

    addsidenav

--- src/app/layout/header.component.ts
+++ src/app/layout/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../_sass/layout.scss']
 })
 export class HeaderComponent implements OnInit {
-
+  visible
   constructor() { }
 
   ngOnInit(): void {
