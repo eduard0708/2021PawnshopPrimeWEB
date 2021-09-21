6c39a0ef0e0c8720d6352b4a5160aa5a95e3305a
eduard0708 <eduard.uytoco@gmail.com>
2021-09-21 23:06:54 +0300

    initCreate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header.component';
+import { SidenavComponent } from './layout/sidenav.component';
+import { FooterComponent } from './layout/footer.component';
+import { NewloanComponent } from './transactions/newloan.component';
+import { RedeemComponent } from './transactions/redeem.component';
+import { RenewComponent } from './transactions/renew.component';
+import { AdditionalComponent } from './transactions/additional.component';
+import { PartialComponent } from './transactions/partial.component';
+import { LoginComponent } from './pages/login.component';
+import { ErrorComponent } from './pages/error.component';
+import { NotfoundComponent } from './pages/notfound.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { CardComponent } from './_cards/card/card.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    FooterComponent,
+    NewloanComponent,
+    RedeemComponent,
+    RenewComponent,
+    AdditionalComponent,
+    PartialComponent,
+    LoginComponent,
+    ErrorComponent,
+    NotfoundComponent,
+    DashboardComponent,
+    CardComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/transactions/renew.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-renew',
+  templateUrl: './renew.component.html'
+})
+export class RenewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
